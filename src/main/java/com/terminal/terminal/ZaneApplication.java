302f898f993a8067bdb0598276eac4fbdc4b120c
DESKTOP-2QFA2IU\gabri <gabrielezanetti.gabriele@gmail.com>
2025-05-05 15:52:35 +0200

    Configurazione

---
+++ src/main/java/com/terminal/terminal/ZaneApplication.java
@@ -0,0 +1,13 @@
+package com.terminal.terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terminal/terminal/ZaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terminal.terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
